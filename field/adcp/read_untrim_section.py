954c79ccdd65eff076cf32003ea92ce0c3528117
Rusty Holleman <rustychris@gmail.com>
2018-06-22 16:25:19 -0700

    Refactoring transect code

--- field/adcp/read_untrim_section.py
+++ field/adcp/read_untrim_section.py
@@ -7,30 +7,34 @@ import pandas as pd
 import numpy as np
 import xarray as xr
 from stompy.spatial import proj_utils
-from stompy import utils
+from stompy import utils,xr_transect
 
 import logging as log
 
-from adcpy import adcpy
+try:
+    from adcpy import adcpy
+except ImportError:
+    log.warning("Failed to import adcpy")
+    adcpy=None
 
 def parse_section_hydro(filename):
     """
     Parse extract_velocity_section output
     """
     all_sections=[]
-    
+
     with open(filename,'rt') as fp:
         line=fp.readline().strip()
 
         while 1: # Loop over transects
             if not line:
                 break
-            
+
             # "section 8B"
             assert line.startswith("section ")
             section_name=line.split(' ')[1]
             line=fp.readline().strip()
-            
+
             water_columns=[]
             while 1: # Loop over locations within one transect:
                 if not line:
@@ -45,7 +49,7 @@ def parse_section_hydro(filename):
                 k_bed=int(k_bed) ; k_surf=int(k_surf)
 
                 bins=[]
-                
+
                 for k in range(k_bed,k_surf+1):
                     # each bin "20 -0.108014 0.015937 0.500000"
                     items=fp.readline().strip().split()
@@ -58,7 +62,7 @@ def parse_section_hydro(filename):
                 water_columns.append(water_column)
 
                 line=fp.readline().strip()
-                
+
             # Close out this section
             all_sections.append( (section_name,water_columns) )
 
@@ -72,7 +76,7 @@ def parsed_to_ds(section,filename):
     ds.attrs['name']=section[0]
     ds.attrs['source']="%s:%s"%(filename,section[0])
     ds.attrs['filename']=filename
-    
+
     profiles=section[1]
 
     wet_profiles=[ prof for prof in profiles if prof['k_bed']<prof['k_surf'] ]
@@ -81,9 +85,9 @@ def parsed_to_ds(section,filename):
 
     ds['z_surf']=('sample',), np.array([p['z_surf'] for p in wet_profiles]) # z_surf is positive up
     ds['z_bed'] =('sample',), np.array([-p['z_bed'] for p in wet_profiles]) # p['z_bed'] is positive down
-    
+
     z_min=ds.z_bed.values.min()
-    z_max=ds.z_surf.values.max() 
+    z_max=ds.z_surf.values.max()
 
     dz_min=0.1 # could scan, but a sliced surface layer might look really small
     # Resample to evenly spaced vertical axis.  Will be shifting to be positive up, relative to
@@ -99,42 +103,31 @@ def parsed_to_ds(section,filename):
     Vu=np.nan*np.ones( (len(ds['sample']),len(ds['z'])), 'f8')
 
     for p_i,p in enumerate(wet_profiles):
-        if 0: # take model outputs at bin centers, interpolate as point measurement
-            # Seems that the bed cells are shaved, so use the surface elevation to get
-            # the true vertical coordinate
-            bin_dz=np.array([ b['dz'] for b in p['bins'] ])
-            bin_center_z=p['z_surf'] - (np.cumsum(bin_dz[::-1])[::-1] - 0.5*bin_dz)
+        # represent discrete cells directly
+        # thickness of each layer in the model output
+        dzs=[ b['dz'] for b in p['bins'] ]
+        # Add a zero thickness at the top to help with going from bins to
+        # interfaces
+        bin_dz=np.array([0] + dzs)
+        # n'th item is the elevation of the bottom of the n'th layer, relative
+        # to the surface, with an extra item for the top of the top layer.
+        # bin_bottoms=p['z_surf'] - (np.cumsum(bin_dz[::-1])[::-1])
+        interface_elevations=(-p['z_bed']) + np.cumsum(bin_dz)
 
-            for tgt,src in [ (Ve,'u'),(Vn,'v') ]:
-                tgt[p_i]=np.interp( z_resamp,
-                                    bin_center_z, [b[src] for b in p['bins']],
-                                    left=np.nan,right=np.nan)
-            Vu[...] = 0*Ve[...] # not reported
-        else: # represent discrete cells directly
-            # thickness of each layer in the model output
-            dzs=[ b['dz'] for b in p['bins'] ]
-            # Add a zero thickness at the top to help with going from bins to
-            # interfaces
-            bin_dz=np.array([0] + dzs)
-            # n'th item is the elevation of the bottom of the n'th layer, relative
-            # to the surface, with an extra item for the top of the top layer.
-            # bin_bottoms=p['z_surf'] - (np.cumsum(bin_dz[::-1])[::-1])
-            interface_elevations=(-p['z_bed']) + np.cumsum(bin_dz)
-            
-            # should be within text tolerances.
-            assert np.abs(interface_elevations[-1] - p['z_surf']) < 0.01
+        # should be within text tolerances.
+        assert np.abs(interface_elevations[-1] - p['z_surf']) < 0.01
 
-            Ninterface=len(interface_elevations)
-            bins=np.searchsorted(interface_elevations,z_resamp)
-            valid=(bins>0) & (bins<Ninterface)
+        Ninterface=len(interface_elevations)
+        bins=np.searchsorted(interface_elevations,z_resamp)
+        valid=(bins>0) & (bins<Ninterface)
 
-            bins=bins.clip(1,Ninterface-1) - 1
-            
-            for tgt,src in [ (Ve,'u'),(Vn,'v') ]:
-                bin_values=np.array([b[src] for b in p['bins']])
-                tgt[p_i]=np.where(valid, bin_values[bins], np.nan)
+        bins=bins.clip(1,Ninterface-1) - 1
 
-            Vu[...] = 0*Ve[...] # not reported
+        for tgt,src in [ (Ve,'u'),(Vn,'v') ]:
+            bin_values=np.array([b[src] for b in p['bins']])
+            tgt[p_i]=np.where(valid, bin_values[bins], np.nan)
+
+        Vu[...] = 0*Ve[...] # not reported
 
     ds['Ve']=('sample','cell'), Ve
     ds['Vn']=('sample','cell'), Vn
@@ -151,98 +144,66 @@ def parsed_to_ds(section,filename):
     ds['lat']=('sample',),ll[:,1]
     return ds
 
+if adcpy is not None:
+    class ADCPSectionHydro(adcpy.ADCPData):
+        """
+        Read one section from a section_hydro.txt file
+        """
+        def __init__(self,ds,**kwargs):
+            super(ADCPSectionHydro,self).__init__(**kwargs)
 
-def section_hydro_to_dss(filename):
-    all_sections=parse_section_hydro(filename)
+            self.name=ds.attrs['name']
+            self.filename=ds.attrs['filename']
 
-    all_ds=[]
+            self.ds=ds # section_hydro_to_ds(filename,name)
+            self.convert_from_ds()
+        def convert_from_ds(self):
+            # Set ADCPData members from self.ds
 
-    for section in all_sections:
-        ds=parsed_to_ds(section,filename)
-        all_ds.append(ds)
-    return all_ds
+            # use round(...,4) to drop some FP roundoff trash
+            # model data comes in with absolute z coordinate, but convert to
+            # depth below surface:
+            _,z_2d = xr.broadcast(self.ds.Ve, self.ds.z_surf-self.ds.location)
+            z_in=z_2d.values # self.ds.location.values
+            valid=np.isfinite(self.ds.Ve.values)
 
-def section_names(filename):
-    all_sections=parse_section_hydro(filename)
-    return [sec[0] for sec in all_sections]
-    
-def section_hydro_to_ds(filename,name=None,index=None):
-    all_sections=parse_section_hydro(filename)
+            min_z=round(np.nanmin(z_in[valid]),4)
+            min_dz=np.round(np.nanmin(np.diff(z_in,axis=1)),4)
+            max_z=round(np.nanmax(z_in[valid]),4)
 
-    if index is not None:
-        if index<len(all_sections):
-            section=all_sections[index]
-        else:
-            return None
-    elif name is not None:        
-        for section in all_sections:
-            if name is not None and section[0]==name:
-                break
-        else:
-            return None
+            dz_sgn=np.sign(min_dz)
+            min_dz=np.abs(min_dz)
 
-    return parsed_to_ds(section,filename)
+            nbins=1+int(round( (max_z-min_z)/min_dz))
+            new_z=np.linspace(min_z,max_z,nbins)
 
-class ADCPSectionHydro(adcpy.ADCPData):
-    """
-    Read one section from a section_hydro.txt file
-    """
-    
-    def __init__(self,ds,**kwargs):
-        super(ADCPSectionHydro,self).__init__(**kwargs)
+            def resamp(orig,axis=-1):
+                """ orig: [samples,cells].
+                interpolate each sample to from z_in[sample,:] to new_z
+                """
+                n_samples=orig.shape[0]
+                new_A=np.zeros( (n_samples,len(new_z)),np.float64)
+                for sample in range(n_samples):
+                    new_A[sample,:]= np.interp(dz_sgn*new_z,
+                                               dz_sgn*z_in[sample,:],orig[sample,:],
+                                               left=np.nan,right=np.nan)
+                return new_A
 
-        self.name=ds.attrs['name']
-        self.filename=ds.attrs['filename']
-        
-        self.ds=ds # section_hydro_to_ds(filename,name)
-        self.convert_from_ds()
-    def convert_from_ds(self):
-        # Set ADCPData members from self.ds
+            self.n_ensembles=len(self.ds.sample)
+            self.velocity=np.array( (resamp(self.ds.Ve.values),
+                                     resamp(self.ds.Vn.values),
+                                     resamp(self.ds.Vu.values)) ).transpose(1,2,0)
+            self.bin_center_elevation=-new_z # make it negative downward
+            self.n_bins=len(new_z)
+            if 'time' in self.ds:
+                self.mtime=utils.to_dnum(self.ds.time.values)
+            else:
+                mtime=utils.to_dnum(np.datetime64("2000-01-01"))
+                self.mtime=mtime * np.ones(len(self.ds.sample))
 
-        # use round(...,4) to drop some FP roundoff trash
-        # model data comes in with absolute z coordinate, but convert to
-        # depth below surface:
-        _,z_2d = xr.broadcast(self.ds.Ve, self.ds.z_surf-self.ds.location)
-        z_in=z_2d.values # self.ds.location.values
-        valid=np.isfinite(self.ds.Ve.values)
-
-        min_z=round(np.nanmin(z_in[valid]),4)
-        min_dz=np.round(np.nanmin(np.diff(z_in,axis=1)),4)
-        max_z=round(np.nanmax(z_in[valid]),4)
-
-        dz_sgn=np.sign(min_dz)
-        min_dz=np.abs(min_dz)
-
-        nbins=1+int(round( (max_z-min_z)/min_dz))
-        new_z=np.linspace(min_z,max_z,nbins)
-
-        def resamp(orig,axis=-1):
-            """ orig: [samples,cells].
-            interpolate each sample to from z_in[sample,:] to new_z
-            """
-            n_samples=orig.shape[0]
-            new_A=np.zeros( (n_samples,len(new_z)),np.float64)
-            for sample in range(n_samples):
-                new_A[sample,:]= np.interp(dz_sgn*new_z,
-                                           dz_sgn*z_in[sample,:],orig[sample,:],
-                                           left=np.nan,right=np.nan)
-            return new_A
-
-        self.n_ensembles=len(self.ds.sample)
-        self.velocity=np.array( (resamp(self.ds.Ve.values),
-                                 resamp(self.ds.Vn.values),
-                                 resamp(self.ds.Vu.values)) ).transpose(1,2,0)
-        self.bin_center_elevation=-new_z # make it negative downward
-        self.n_bins=len(new_z)
-        if 'time' in self.ds:
-            self.mtime=utils.to_dnum(self.ds.time.values)
-        else:
-            mtime=utils.to_dnum(np.datetime64("2000-01-01"))
-            self.mtime=mtime * np.ones(len(self.ds.sample))
-                
-        self.rotation_angle=0
-        self.rotation_axes=0
-        self.lonlat=np.c_[self.ds.lon.values,self.ds.lat.values]
-        self.source=self.ds.attrs['source'] # self.filename
-        self.name=self.name
-        self.references="UnTRIM"
+            self.rotation_angle=0
+            self.rotation_axes=0
+            self.lonlat=np.c_[self.ds.lon.values,self.ds.lat.values]
+            self.source=self.ds.attrs['source'] # self.filename
+            self.name=self.name
+            self.references="UnTRIM"
--- model/dfm/dfm/plots_v00.py
+++ model/dfm/dfm/plots_v00.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
 import xarray as xr
 from stompy.model.delft import dfm_grid
 from stompy.plot import plot_utils
+import stompy.plot.cmap as scmap
+
 
 ##
 
-ds=xr.open_dataset('runs/hor_002/DFM_OUTPUT_flowfm/flowfm_20120801_000000_map.nc')
+ds=xr.open_dataset('runs/hor_002/DFM_OUTPUT_flowfm/flowfm_0000_20120801_000000_map.nc')
 
 g=dfm_grid.DFMGrid(ds)
 
@@ -16,7 +20,7 @@ plt.figure(1).clf()
 fig,ax=plt.subplots(num=1)
 
 ccoll=g.plot_cells(ax=ax,values=ds.s1.isel(time=-1),cmap='jet')
-ccoll.set_clim([1.9,2.2])
+ccoll.set_clim([1.9,2.5])
 
 plot_utils.cbar(ccoll)
 ax.axis('equal')
@@ -55,8 +59,159 @@ plt.figure(3).clf()
 fig,ax=plt.subplots(num=3)
 ax.plot(ds.time.values, c_mag)
 
+##
+
+# The 3D run is roughly at steady state by 4h, with a little
+# bit of adjusting / sloshing evident.
+
+# Extract some transects.
+# First, read Ed's transects:
+untrim_sections_fn="../../untrim/ed-steady/section_hydro.txt"
+
+from stompy import utils
+utils.path("../../../field/adcp/")
+
+import read_untrim_section
+
+untrim_sections=read_untrim_section.section_hydro_to_dss(untrim_sections_fn)
+
+##
+
+# Find 7B for a nice comparison.
+sec=[ sec for sec in untrim_sections if sec.attrs['name']=='7B'][0]
+
+##
+
+import stompy.model.delft.dflow_model as dfm
+from stompy import xr_utils
+import six
+six.moves.reload_module(xr_utils)
+six.moves.reload_module(dfm)
+
+line_xy=np.c_[sec.x_utm.values, sec.y_utm.values]
+
+prof=dfm.extract_transect(ds.isel(time=-1),line=line_xy,dx=3,grid=g)
+
+xr_utils.bundle_components(prof,'U',['Ve','Vn'],'en',['E','N'])
+xr_utils.bundle_components(prof,'U_avg',['Ve_avg','Vn_avg'],'en',['E','N'])
+
+##
+
+untrim_sec=xr_transect.section_hydro_to_transect(untrim_sections_fn,name="7B")
+
+##
+
+aprof=prof.copy()
+
+aprof.attrs['name']=sec.attrs['name']
+aprof.attrs['filename']='hor002'
+aprof.rename( {'s1':'z_surf'}, inplace=True)
+prof_adcp=read_untrim_section.ADCPSectionHydro(aprof)
+
+##
+
+plt.figure(2).clf()
+fig,ax=plt.subplots(num=2)
+
+ax.quiver( prof.x_sample, prof.y_sample,
+           prof.U_avg.sel(en='E'), prof.U_avg.sel(en='N'))
+
+##
+
+# Make sure the new plot code does something similar:
+plt.figure(3).clf()
+fig,axs=plt.subplots(2,1,num=3,sharex=True,sharey=True)
+
+coll_u=xr_transect.plot_scalar(prof,prof.U.sel(en='E'),ax=axs[0])
+coll_v=xr_transect.plot_scalar(prof,prof.U.sel(en='N'),ax=axs[1])
+
+plt.setp([coll_u,coll_v], clim=[-0.5,0.5],cmap=cmap_sym)
+for ax,coll,label in zip(axs,
+                         [coll_u,coll_v],
+                         ["U (m/s","V (m/s)"]):
+    plt.colorbar(coll,ax=ax,label=label)
+plt.setp(axs,facecolor='0.7')
+
+##
+from stompy import xr_transect
+six.moves.reload_module(xr_transect)
+
+prof['Q']=(),xr_transect.Qleft(prof)
+
+xr_transect.add_rozovski(prof,'U','Uroz')
+
+##
+
+# Scatter with real z coordinate
+plt.figure(4).clf()
+fig,axs=plt.subplots(2,1,num=4,sharex=True,sharey=True)
+
+x,y,Cu,Cv=xr.broadcast(prof.d_sample,prof.z_ctr,
+                       prof.Uroz.isel(roz=0),prof.Uroz.isel(roz=1))
+coll_u=plot_utils.pad_pcolormesh(x,y,Cu,ax=axs[0])
+coll_v=plot_utils.pad_pcolormesh(x,y,Cv,ax=axs[1])
 
 
+coll_u.set_clim([0,1.0])
+coll_u.set_cmap(cmap_mag)
+coll_v.set_clim([-0.1,0.1])
+coll_v.set_cmap(cmap_sym)
 
+for ax,coll,label in zip(axs,
+                         [coll_u,coll_v],
+                         ["U (m/s","V (m/s)"]):
+    plt.colorbar(coll,ax=ax,label=label)
 
+plt.setp(axs,facecolor='0.7')
+
+##
+
+# should push this to the read_untrim_section code
+sec=sec.rename({
+    'x_utm':'x_sample',
+    'y_utm':'y_sample',
+    'z':'z_ctr',
+    'cell':'laydim'
+})
+
+##
+# Is it really that hard to get ADCPy working?
+# no, but it's not the right data model for any of the models
+# or the Sontek ADCP.
+six.moves.reload_module(xr_transect)
+
+# Do this stuff again, but with the untrim data:
+xr_utils.bundle_components(sec,'U',['Ve','Vn'],'en',['E','N'])
+
+sec['U_avg']=xr_transect.depth_avg(sec,'U')
+sec['d_sample']=('sample',), utils.dist_along( np.c_[sec.x_sample,sec.y_sample])
+
+##
+plt.figure(12).clf()
+fig,ax=plt.subplots(num=12)
+
+ax.quiver( sec.x_sample, sec.y_sample,
+           sec.U_avg.sel(en='E'), sec.U_avg.sel(en='N'))
+
+##
+
+# Scatter with real z coordinate
+plt.figure(13).clf()
+fig,axs=plt.subplots(2,1,num=13,sharex=True,sharey=True)
+
+x,y,Cu,Cv=xr.broadcast(sec.d_sample,sec.z_ctr,
+                       sec.U.sel(en='E'),
+                       sec.U.sel(en='N'))
+coll_u=plot_utils.pad_pcolormesh(x.values,y.values,Cu.values,ax=axs[0])
+coll_v=plot_utils.pad_pcolormesh(x.values,y.values,Cv.values,ax=axs[1])
+
+plt.setp([coll_u,coll_v], clim=[-0.5,0.5],cmap=cmap_sym)
+for ax,coll,label in zip(axs,
+                         [coll_u,coll_v],
+                         ["U (m/s","V (m/s)"]):
+    plt.colorbar(coll,ax=ax,label=label)
+
+plt.setp(axs,facecolor='0.7')
+
+# HERE - basic plot works..
 
