47f55a97b371cad5709d574f3df57c3b85b4f71e
Rusty Chris Holleman <cdholleman@ucdavis.edu>
2021-12-08 14:01:46 -0800

    Bathy changes for large grid

--- bathy/bathy.py
+++ bathy/bathy.py
@@ -2,26 +2,36 @@ import os
 from stompy.spatial import field
 from stompy import memoize
 
+here=os.path.dirname(__file__)
+
 @memoize.memoize()
 def dem(suffix=''):
+    # This DEM covers a larger area, but doesn't have the extra processing
+    # around the junction
+    base_fn=os.path.join(here,"junction-composite-dem.tif")
+
     if suffix=='':
-        return field.GdalGrid(os.path.join(os.path.dirname(__file__),
-                                           'junction-composite-20190117-no_adcp.tif'))
+        jct_fn=os.path.join(here,'junction-composite-20190117-no_adcp.tif')
     elif suffix=='smooth':
-        return field.GdalGrid(os.path.join(os.path.dirname(__file__),
-                                           'junction-composite-20200603-w_smooth.tif'))
+        jct_fn=os.path.join(here,'junction-composite-20200603-w_smooth.tif')
     elif suffix=='smoothadcp':
-        return field.GdalGrid(os.path.join(os.path.dirname(__file__),
-                                           'junction-composite-20200604-w_smooth.tif'))
+        jct_fn=os.path.join(here,'junction-composite-20200604-w_smooth.tif')
     elif suffix=='smoothadcp2':
         # this includes adcp-derived bathy above the junction, while
         # the above uses dwr multibeam above the junction
-        return field.GdalGrid(os.path.join(os.path.dirname(__file__),
-                                           'junction-composite-20200605-w_smooth.tif'))
+        jct_fn=os.path.join(here,'junction-composite-20200605-w_smooth.tif')
     elif suffix=='adcp':
-        return field.GdalGrid(os.path.join(os.path.dirname(__file__),
-                                           'junction-composite-dem.tif'))
+        jct_fn=None
     else:
         raise Exception("Unknown bathy suffix/version: %s"%suffix)
+
+    if jct_fn is not None:
+        # Specify priority to be sure jct fn wins out
+        f=field.MultiRasterField([(jct_fn,10),
+                                  (base_fn,0)])
+    else:
+        f=field.GdalGrid(base_fn)
+        
+    return f
 
 
